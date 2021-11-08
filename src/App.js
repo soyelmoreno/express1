0a0b8340f548398bf5e9152155428887f453446a
Carlos Moreno <soyelmoreno@gmail.com>
2021-11-08 00:17:04 -0800

    Add a small form that fetches product data via proxy to API server

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [handle, setHandle] = useState('');
+  const [result, setResult] = useState({});
+
+  async function handleClick() {
+    const response = await fetch(`/api/products?handle=${handle}`);
+    const data = await response.json();
+    const dataProcessed = JSON.stringify(data, null, 2);
+    debugger;
+    setResult(dataProcessed);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <label>Enter product handle</label>
+        <input
+          type="text"
+          value={handle}
+          onChange={(e) => setHandle(e.target.value)}
+        />
+        <button type="button" style={{ padding: '1rem' }} onClick={handleClick}>
+          Fetch products
+        </button>
+
+        <label>Result</label>
+        <textarea
+          style={{ width: '95%', height: '25rem' }}
+          className="result"
+          value={result}
+          onChange={(e) => setResult(e.target.value)}
+        />
       </header>
     </div>
   );
